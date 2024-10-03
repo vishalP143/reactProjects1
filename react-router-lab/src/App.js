cb0ea52fdb5263e0c47e5118a11f21695b88d655
vishalP143 <vishal.p8070@gmail.com>
2024-10-03 10:22:01 +0000

    oo

--- react-router-lab/src/App.js
+++ react-router-lab/src/App.js
@@ -10,10 +10,10 @@ import './App.css'; // Parent styles
 
 const App = () => {
     return (
-        <Router>(This component wraps your entire application and enables React Router. It listens to the browser's address bar and synchronizes the UI with the current URL.)
+        <Router>
             <div className="box-container">
-                <Routes>(The Routes component is used to define all the different routes (URLs) of your app. Each route renders a different component based on the path in the URL.)
-                    <Route path="/" element={<PersonList />} />(The Route component defines a specific route in your app. When the URL matches the path specified, it renders the component passed to the element prop.)
+                <Routes>
+                    <Route path="/" element={<PersonList />} />
                     <Route path="/add" element={<PersonAdd />} />
                     <Route path="/edit/:id" element={<PersonEdit />} />
                     <Route path="/person/:id" element={<PersonDetail />} />
